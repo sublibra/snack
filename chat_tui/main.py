b92da552fb3cbeec9de77eff9986199fac4e2dba
Jonas Hörström <jonas.horstrom@gmail.com>
2025-10-19 10:44:12 +0200

    Review comments again

--- chat_tui/main.py
+++ chat_tui/main.py
@@ -118,7 +118,7 @@ class ChatTUIApp(App):
 
     def __init__(self):
         super().__init__()
-        self.api_client = SnackAPIClient(base_url=os.environ.get("SERVER_URL", "localhost:8000"))
+        self.api_client = SnackAPIClient(base_url=os.environ.get("SERVER_URL", "http://localhost:8000"))
         self.current_chat_messages = []
         self.notification_sound = True
         
